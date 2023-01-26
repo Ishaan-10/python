df2a67a87aca923533796c7a37ca9ed162c080b6
Ishaan-10 <b.ishaan10@gmail.com>
2023-01-26 23:41:49 +0530

    loops,conditionals

---
+++ Basics/conditional.py
@@ -0,0 +1,15 @@
+# is_true=False
+
+# if is_true:
+#     print("True")
+# elif not(is_true):
+#     print("False")
+
+good_personality=False
+good_looks=True
+if good_personality and good_looks:
+    print("Perfect")
+elif good_personality or good_looks:
+    print("Good")
+else:
+    print("Bad")
---
+++ Basics/loops.py
@@ -0,0 +1,25 @@
+# While
+i=1
+while i<=10:
+    print('*'*i)
+    i+=1
+    if i==5: break;
+else:
+    print("Whole while loop ran")
+print()
+i=1
+while i<=10:
+    print('*'*i)
+    i+=1
+else:
+    print("Whole while loop ran")
+
+# For
+for c in "String":
+    print(c)
+
+for i in range(0,5):
+    print(i)
+
+for item in ["hi","hello","namaste"]:
+    print(item)
